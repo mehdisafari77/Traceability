10da4cdf7c7cd109293d3d67354944bff2651649
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-01-27 13:20:36 -0800

    add server setup and tested

---
+++ server/server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const path = require("path");
+const Rollbar = require("rollbar")
+
+let rollbar = new Rollbar({
+  accessToken: '905e58b2bbe740af8cafcd01e7f553a7',
+  captureUncaught: true,
+  captureUnhandledRejections: true
+})
+
+const app = express();
+app.use(express.json())
+
+app.use('/js', express.static(path.join(__dirname, '../public/main.js')))
+app.use('/axios', express.static(path.join(__dirname, "./node_modules/axios/dist/axios.min.js")))
+
+app.get("/", function(req, res){
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+
+  rollbar.info('HTML file served successfully.')
+  
+  try {
+    nonExistentFunction();
+  } catch (error) {
+    rollbar.error(error);
+  }
+});
+
+const port = process.env.PORT || 4005;
+
+app.use(rollbar.errorHandler())
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+})
